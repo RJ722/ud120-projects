1c914a14b84a27de8295e3d191fb944dc4693fd9
RJ722 <rahul722j@gmail.com>
2017-11-18 18:14:57 +0530

    Complete Lesson 1 Assignment

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -22,12 +22,23 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+def train(features_train, labels_train):
+    t0 = time()
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time()-t0, 3), "s"
+    return clf
 
 
-#########################################################
-### your code goes here ###
+def test(clf, features_test, labels_test):
+    return clf.score(features_test, labels_test)
 
 
-#########################################################
+def main():
+    clf = train(features_train, labels_train)
+    ## Check for accuracy of the classifier
+    print(test(clf, features_test, labels_test))
 
 
+if __name__ == '__main__':
+    main()
